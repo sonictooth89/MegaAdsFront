42d85b21ff4d76581a005a268a07535279dddafc
Malgorzata Nowak <nowak.em@interia.eu>
2023-02-02 20:58:50 +0100

    Basic structore of main view.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export const App = () => {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>
+          <p><strong>Mega</strong> Ogloszenia</p>
+        </h1>
+        <button>Dodaj Ogloszenie</button>
+        <div className="search">
+          <input type="text" /><button>Szukaj</button>
+        </div>
       </header>
-    </div>
+      <div className="map">
+        ...
+      </div>
+    </>
   );
 }
 
